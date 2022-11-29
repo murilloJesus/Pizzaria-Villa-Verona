91b7e43f36be34ab6382954cefda1e73823629dd
Leonardo Ortiz <leoortizinho23@gmail.com>
2022-11-29 16:34:35 -0300

    primeiro commit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,20 @@
+
+<link rel="stylesheet" href="/assets/css/footer.css">
+		<footer class="footer-distributed">
+
+			<div class="footer-right">
+
+				<a href="https://www.facebook.com/profile.php?id=100086372554129"><img src="/assets/images/icon-facebook.png" alt="Icone"> </a>
+                <a href="https://www.instagram.com/pizzariavillaverona/"><img src="/assets/images/icon-insta.jpeg"  alt="Icone"> </a>
+                <a href="https://api.whatsapp.com/send?phone=5515996979953&text=Ol%C3%A1%20Gostaria%20de%20fazer%20meu%20pedido"><img src="/assets/images/icon-wats.png"  alt="Icone"> </a>
+				
+
+			</div>
+
+			<div class="footer-left">
+
+				
+				<p>Desing por: Murillo (ALTERAR CASO PRECISE) &copy; Direitos: Pizzaria Villa Verona</p>
+			</div>
+
+		</footer>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Villa Verona</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nothing+You+Could+Do" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/animate.css">
+    <link rel="stylesheet" href="/assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="/assets/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="/assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="/assets/css/aos.css">
+    <link rel="stylesheet" href="/assets/css/ionicons.min.css">
+    <link rel="stylesheet" href="/assets/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="/assets/css/jquery.timepicker.css">
+    <link rel="stylesheet" href="/assets/css/flaticon.css">
+    <link rel="stylesheet" href="/assets/css/icomoon.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
+  </head>
+  <body>
+  <audio src="/assets/audios/audio-fornalha.mp3" autoplay></audio>
+  
+  <x-menu />
+  
+  <!-- menu.php -->
+
+    <section class="home-slider owl-carousel img" style="background-image: url(/assets/images/bg_1.jpg);">
+      <div class="slider-item">
+      	<div class="overlay"></div>
+        <div class="container">
+          <div class="row slider-text align-items-center" data-scrollax-parent="true">
+
+            <div class="col-md-6 col-sm-12 ftco-animate">
+            	<span class="subheading">Todo dia é dia de Pizza</span>
+              <h1 class="mb-4">VILLA VERONA PIZZARIA</h1>
+              <p class="mb-4 mb-md-5">Que nunca nos falte paz, amor, felicidade e dinheiro para comprar pizza!</p>
+              <a href="/assets/cardapio.blade.php" class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">Acessar Nosso Menu</a></p>
+            </div>
+            <div class="col-md-6 ftco-animate">
+            	<img src="/assets/images/bg_1.png" class="img-fluid" alt="">
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="slider-item">
+      	<div class="overlay"></div>
+        <div class="container">
+          <div class="row slider-text align-items-center" data-scrollax-parent="true">
+
+            <div class="col-md-6 col-sm-12 order-md-last ftco-animate">
+            	<span class="subheading">Bora Comer PIzza?</span>
+              <h1 class="mb-4">VILLA VERONA PIZZARIA</h1>
+              <p class="mb-4 mb-md-5">Salgada ou doce, com ou sem borda, sendo pizza eu amo de qualquer jeito!</p>
+			  <a href="/assets/cardapio.blade.php" class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">Acessar Nosso Menu</a></p>
+			</div>
+            <div class="col-md-6 ftco-animate">
+            	<img src="/assets/images/bg_2.png" class="img-fluid" alt="">
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      
+    </section>
+
+    <section class="ftco-intro">
+    	<div class="container-wrap">
+    		<div class="wrap d-md-flex">
+	    		<div class="info">
+	    			<div class="row no-gutters">
+	    				<div class="col-md-4 d-flex ftco-animate" id="sobre-nos">
+	    					<div class="icon"><span class="icon-phone"></span></div>
+	    					<div class="text">
+	    						<h3>Entre em contato</h3>
+	    						<p>(15) 99697-9953</p>
+	    					</div>
+	    				</div>
+	    				<div class="col-md-4 d-flex ftco-animate">
+	    					<div class="icon"><span class="icon-my_location"></span></div>
+	    					<div class="text">
+	    						<h3>Nosso Endereço</h3>
+	    						<p>R. Trajano Píres 62 - Jardim Guadalajara </p>
+	    					</div>
+	    				</div>
+	    				<div class="col-md-4 d-flex ftco-animate">
+	    					<div class="icon"><span class="icon-clock-o"></span></div>
+	    					<div class="text">
+	    						<h3> abertos de Seg a sex</h3>
+	    						<p>11:00 as 14:00 / 19:00 as 22:30</p>
+	    					</div>
+	    				</div>
+	    			</div>
+	    		
+    	</div>
+    </section>
+
+    <section class="ftco-about d-md-flex">
+    	<div class="one-half img" style="background-image: url(/assets/images/about.jpg);"></div>
+    	<div class="one-half ftco-animate">
+        <div class="heading-section ftco-animate ">
+          <h2 class="mb-4">Bem Vindo a <span class="flaticon-pizza">&nbsp; Pizzaria</span> <br> Villa Verona </h2>
+        </div>
+        <div>
+  				<p>Seja Bem Vindo a Pizzaria Villa Verona onde é sempre um prazer poder atende-lo, Fique a vontade para olhar nosso cardapio <br>é sempre bom ressaltar que sua opinião e muito importante para nós </p>
+  			</div>
+    	</div>
+    </section>
+
+    <section class="ftco-section">
+    	<div class="container">
+    		<div class="row justify-content-center mb-5 pb-3">
+          <div class="col-md-7 heading-section ftco-animate text-center">
+            <h2 class="mb-4">Os Queridinhos da Casa</h2>
+            <p>Estas são algumas das pizzas mais pedidas da casa.</p>
+          </div>
+        </div>
+    	</div>
+
+
+
+      <!-- itens queridinhos da casa -->
+
+    	<div class="container-wrap">
+    		<div class="row no-gutters d-flex">
+    			<div class="col-lg-4 d-flex ftco-animate">
+    				<div class="services-wrap d-flex">
+    					<a href="https://api.whatsapp.com/send?phone=5515996979953&text=Oi%20tudo%20bem%3F%20Gostaria%20de%20uma%20pizza%20de%20Batata%20Frita%20de%208%20peda%C3%A7os" class="img" style="background-image: url(/assets/images/pizza-batata-frita.png);"></a>
+    					<div class="text p-4">
+    						<h3>Pizza: Batata Frita</h3>
+    						<p> Ingredientes: Mussarela - Requeijão tipo Catupiry - Bacon - Batata Frita - Pizza de 8 pedaços</p>
+    						<p class="price"><span>R$48.50</span></p>
+							
+    					</div>
+    				</div>
+    			</div>
+
+          <div class="col-lg-4 d-flex ftco-animate">
+    				<div class="services-wrap d-flex">
+    					<a href="
+                https://api.whatsapp.com/send?phone=5515996979953&text=Oi%20tudo%20bem%3F%20Gostaria%20de%20uma%20pizza%20%20Portuguesa%20de%208%20peda%C3%A7os" class="img" style="background-image: url(/assets/images/pizza-queijo.png);"></a>
+    					<div class="text p-4">
+    						<h3>Pizza: 4 Queijos</h3>
+    						<p>Ingredientes:  Mussarela - Requeijão tipo Catupiry - Parmesão - Provolone - Pizza de 8 pedaços </p>
+    						<p class="price"><span>R$56.60</span></p>
+    					</div>
+    				</div>
+    			</div>
+
+
+				<div class="col-lg-4 d-flex ftco-animate">
+    				<div class="services-wrap d-flex">
+    					<a href="   https://api.whatsapp.com/send?phone=5515996979953&text=Oi%20tudo%20bem%3F%20Gostaria%20de%20uma%20pizza%20de%20%204%20Queijos%20de%208%20peda%C3%A7os" class="img" style="background-image: url(/assets/images/pizza-portuguesa.png);"></a>
+    					<div class="text p-4">
+							<h3>Pizza: Portuguesa</h3>
+    						<p>Ingredientes: Mussarela - Presunto - Palmito - Ervilha - Ovo - Cebola - Pizza de 8 pedaços </p>
+    						<p class="price"><span>R$55.40</span> </p>
+    					</div>
+    				</div>
+    			</div>
+
+				<div class="container-wrap">
+    		<div class="row no-gutters d-flex">
+    			<div class="col-lg-4 d-flex ftco-animate">
+    				<div class="services-wrap d-flex">
+    					<a href="https://api.whatsapp.com/send?phone=5515996979953&text=Oi%20tudo%20bem%3F%20Gostaria%20de%20uma%20pizza%20%20de%20Brigadeiro%20de%208%20peda%C3%A7os" class="img" style="background-image: url(/assets/images/pizza-brigadeiro.png);"></a>
+    					<div class="text p-4">
+    						<h3>Pizza: Brigadeiro </h3>
+    						<p>Ingredientes: Chocolate ao leite - Granulado - Pizza de 8 pedaços   </p>
+    						<p class="price"><span>R$48.30</span> </p>
+							
+    					</div>
+    				</div>
+    			</div>
+          <div class="col-lg-4 d-flex ftco-animate">
+    				<div class="services-wrap d-flex">
+    					<a href="https://api.whatsapp.com/send?phone=5515996979953&text=Oi%20tudo%20bem%3F%20Gostaria%20de%20uma%20pizza%20%20de%20%20Romeu%20e%20Julieta%20de%208%20peda%C3%A7os" class="img" style="background-image: url(/assets/images/pizza-romeu.png);"></a>
+    					<div class="text p-4">
+    						<h3>Pizza: Romeu e Julieta </h3>
+    						<p>Ingredientes: Mussarela e Goiabada - Pizza de 8 pedaços </p>
+    						<p class="price"><span>R$48.30 </span> </p>
+							
+    					</div>
+    				</div>
+    			</div>
+
+    		
+
+				<div class="col-lg-4 d-flex ftco-animate">
+    				<div class="services-wrap d-flex">
+    					<a href="https://api.whatsapp.com/send?phone=5515996979953&text=Oi%20tudo%20bem%3F%20Gostaria%20de%20uma%20pizza%20%20de%20Banana%20com%20chocolate%20de%208%20peda%C3%A7os" class="img" style="background-image: url(/assets/images/pizza-banana.png);"></a>
+    					<div class="text p-4">
+						<h3>Pizza: Banana com chocolate</h3>
+    						<p>Ingredientes: Banana - Chocolate ao Leite - Pizza de 8 pedaços </p>
+    						<p class="price"><span>R$49.50 </span> </p>
+							
+    					</div>
+    				</div>
+    			</div>
+				
+
+       
+  <!-- fim   quridinhos -->
+
+
+ <x-footer />
+
+
+
+  
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="/assets/js/jquery.min.js"></script>
+  <script src="/assets/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="/assets/js/popper.min.js"></script>
+  <script src="/assets/js/bootstrap.min.js"></script>
+  <script src="/assets/js/jquery.easing.1.3.js"></script>
+  <script src="/assets/js/jquery.waypoints.min.js"></script>
+  <script src="/assets/js/jquery.stellar.min.js"></script>
+  <script src="/assets/js/owl.carousel.min.js"></script>
+  <script src="/assets/js/jquery.magnific-popup.min.js"></script>
+  <script src="/assets/js/aos.js"></script>
+  <script src="/assets/js/jquery.animateNumber.min.js"></script>
+  <script src="/assets/js/bootstrap-datepicker.js"></script>
+  <script src="/assets/js/jquery.timepicker.min.js"></script>
+  <script src="/assets/js/scrollax.min.js"></script>
+  <script src="/assets/https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="/assets/js/google-map.js"></script>
+  <script src="/assets/js/main.js"></script>
+    
+  </body>
+</html>
